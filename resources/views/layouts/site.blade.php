75ad27226275f8f1c22f99fd27a0033a43c01b0a
Zaid Shahid <144483692+ZaidShahid15@users.noreply.github.com>
2026-04-22 20:02:34 +0500

    CHANGE

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -180,5 +180,83 @@
         });
     </script>
     @yield('scripts')
+    <script>
+        window.addEventListener('load', function () {
+            if (!window.jQuery || !window.jQuery.fn || typeof window.jQuery.fn.slick !== 'function') {
+                return;
+            }
+
+            var $ = window.jQuery;
+
+            $('.themeholy-carousel').each(function () {
+                var $carousel = $(this);
+
+                if ($carousel.hasClass('slick-initialized')) {
+                    return;
+                }
+
+                var slideShow = parseInt($carousel.attr('data-slide-show') || '1', 10);
+                var scrollCount = parseInt($carousel.attr('data-slide-scroll') || '1', 10);
+                var lgShow = parseInt($carousel.attr('data-lg-slide-show') || slideShow, 10);
+                var mdShow = parseInt($carousel.attr('data-md-slide-show') || lgShow, 10);
+                var smShow = parseInt($carousel.attr('data-sm-slide-show') || mdShow, 10);
+                var xsShow = parseInt($carousel.attr('data-xs-slide-show') || '1', 10);
+                var arrows = $carousel.attr('data-arrows') === 'true';
+                var dots = $carousel.attr('data-dots') === 'true';
+                var autoplay = $carousel.attr('data-autoplay') === 'true';
+                var fade = $carousel.attr('data-fade') === 'true';
+                var centerMode = $carousel.attr('data-center-mode') === 'true';
+                var adaptiveHeight = $carousel.attr('data-adaptive-height') === 'true';
+                var focusOnSelect = $carousel.attr('data-focuson-select') === 'true';
+
+                $carousel.slick({
+                    slidesToShow: slideShow,
+                    slidesToScroll: scrollCount,
+                    infinite: true,
+                    arrows: arrows,
+                    dots: dots,
+                    autoplay: autoplay,
+                    autoplaySpeed: 6000,
+                    speed: 1000,
+                    fade: fade,
+                    centerMode: centerMode,
+                    adaptiveHeight: adaptiveHeight,
+                    focusOnSelect: focusOnSelect,
+                    prevArrow: '<button type="button" class="slick-prev slick-arrow" aria-label="Previous slide"><i class="fal fa-arrow-left"></i></button>',
+                    nextArrow: '<button type="button" class="slick-next slick-arrow" aria-label="Next slide"><i class="fal fa-arrow-right"></i></button>',
+                    responsive: [
+                        {
+                            breakpoint: 1400,
+                            settings: {
+                                slidesToShow: lgShow,
+                                slidesToScroll: Math.min(scrollCount, lgShow)
+                            }
+                        },
+                        {
+                            breakpoint: 1200,
+                            settings: {
+                                slidesToShow: mdShow,
+                                slidesToScroll: Math.min(scrollCount, mdShow)
+                            }
+                        },
+                        {
+                            breakpoint: 992,
+                            settings: {
+                                slidesToShow: smShow,
+                                slidesToScroll: Math.min(scrollCount, smShow)
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: xsShow,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
+            });
+        });
+    </script>
 </body>
 </html>
